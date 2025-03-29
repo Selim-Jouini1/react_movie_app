8a684d254b925464fdc03cb3a7c95c8bfc9e0d72
Selim <s.jouini.work@gmail.com>
2025-03-29 03:30:08 +0100

    Refactor doc structure and configuring app tabs and movies

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,41 @@
-import { Tabs } from 'expo-router';
+import { View, Text, ImageBackgroundBase } from 'react-native';
 import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from 'expo-router';
+import { images } from '@/constants/images';
+import { Image, ImageBackground, } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const _layout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    <Tabs>
+      <Tabs.Screen name="index" 
+      options={{ 
+                title: "Home", 
+                headerShown: false,
+                tabBarIcon: ( {focused}) => (
+                  <>
+                    <ImageBackground
+                        source={images.highlight}
+                        className='flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'
+
+                    >
+                      <Image source={images.home}  tintColor='#151312' className='size-5'/>
+                      <Text>Home</Text>
+                    </ImageBackground>
+
+                  </>
+                 )
+                }} />
+
+      <Tabs.Screen name="search" 
+      options={{ title: "Search",headerShown: false }} />
+
+      <Tabs.Screen name="saved" 
+      options={{ title: "Saved",headerShown: false }} />
+
+      <Tabs.Screen name="profile" 
+      options={{ title: "Profile",headerShown: false }} />
+
+    </Tabs>  )
 }
+
+export default _layout
