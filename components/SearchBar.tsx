bc51c81acfafdbacd37f6374fbbc9a513511c6fe
Selim <s.jouini.work@gmail.com>
2025-03-30 07:41:57 +0100

    Finshed configuring index and search tab, along SeachBar Component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 import { icons } from '@/constants/icons'
 
 interface props {
-    onPress: () => void
+    onPress?: () => void
     placeholder: string
+    value?: string
+    onChangeText?: (text: string) => void
 }
 
-const SearchBar = ( {onPress, placeholder}: props) => {
+const SearchBar = ( {onPress, placeholder, value, onChangeText}: props) => {
  
   return (
     <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
@@ -16,8 +18,8 @@ const SearchBar = ( {onPress, placeholder}: props) => {
         <TextInput
             onPress={onPress}
             placeholder={placeholder}
-            value=''
-            onChangeText={() => {}}
+            value={value}
+            onChangeText={onChangeText}
             placeholderTextColor='#A8B5DB'
             className='flex-1 ml-2 text-white'>
 
