5cf26eec6c699d6df7990efb79bf5ca3621d34b1
Selim <s.jouini.work@gmail.com>
2025-03-30 05:43:13 +0100

    Configuring TMDB API and index screen

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { images } from '@/constants/images'
 
 const search = () => {
   return (
-    <View>
-      <Text>search</Text>
+    <View className='flex-1 bg-primary'>
+
     </View>
   )
 }
